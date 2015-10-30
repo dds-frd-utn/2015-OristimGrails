abacbeaa919209ca598196689aeab26cf85c28c0
diegomartin010 <diegomartin010@gmail.com>
2015-10-30 00:58:48 -0300

    Integracion con Postgres

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,7 +50,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.29'
         //runtime 'mysql:mysql-connector-java:5.1.33'
-        // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
+        runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
     }
 
@@ -65,10 +65,11 @@ grails.project.dependency.resolution = {
         compile ":asset-pipeline:2.2.3"
 
         // plugins needed at runtime but not for compilation
-        runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
-//        runtime ":hibernate:3.6.10.18"
+        //runtime ":hibernate4:4.3.10" // or ":hibernate:3.6.10.18"
+        runtime ":hibernate:3.6.10.18"
         runtime ":database-migration:1.4.0"
         runtime ":jquery:1.11.1"
+        
 //        runtime ':db-reverse-engineer:0.5.1'
         // Uncomment these to enable additional asset-pipeline capabilities
         //compile ":sass-asset-pipeline:1.9.0"
