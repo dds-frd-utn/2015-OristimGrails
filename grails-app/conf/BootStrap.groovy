d87547f7a3ef182dffa87c6a5e47160b185c407a
DMColussi <diegomartin010@gmail.com>
2016-12-08 16:29:08 -0300

    Eliminar Cosas inutiles

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,48 @@
+import oristim.*
+import security.*
+import session.*
+import carrito.*
+
 class BootStrap {
 
     def init = { servletContext ->
+    	new Usuario(
+    		username: 'mrios'
+			,password: 'mrios1'
+			,nombre: 'Matias'
+			,apellido: 'Rios'
+			,email: 'mrios1@gmail.com'
+    	).save()
+    	new Juego(
+    		alias: 'COD'
+			,categoria: 'FPS'
+			,descripcion: "Descripcion del nuevo Juego."
+			,imgroute: "img/imgDfl.jpg"
+			,informacion: "Informacion no cargada."
+			,nombre: 'Call of duty 4'
+			,plataforma: 'PC'
+			,precio: 500
+    	).save()
+    	new Juego(
+    		alias: 'WOW'
+			,categoria: 'FPS'
+			,descripcion: "Descripcion del nuevo Juego."
+			,imgroute: "img/imgDfl.jpg"
+			,informacion: "Informacion no cargada."
+			,nombre: 'Wolrld of warcraft'
+			,plataforma: 'PC'
+			,precio: 500
+    	).save()
+    	new Juego(
+    		alias: 'ASC'
+			,categoria: 'FPS'
+			,descripcion: "Descripcion del nuevo Juego."
+			,imgroute: "img/imgDfl.jpg"
+			,informacion: "Informacion no cargada."
+			,nombre: 'Assasins Creed 4'
+			,plataforma: 'PC'
+			,precio: 500
+    	).save()
     }
     def destroy = {
     }
